b2921ca17488847161138cb6893ae32abe5de21a
MuYaHong2 <93328198+MuYaHong2@users.noreply.github.com>
2021-11-21 20:46:40 +0900

    Test

---
+++ Assets/Starting.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Starting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("했습니다");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
